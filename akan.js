2a7b0fac20f353b45b742565cfb11cda5f8549d9
Halim tullah <issackhalima@gmail.com>
2020-04-01 14:26:01 +0300

    add everything

--- akan.js
+++ akan.js
@@ -1,23 +1,16 @@
-var century, year, month, dayOfMonth, dayOfWeek, day;
-//Get input
-function getInput() {
-    century = parseInt(document.getElementById("century").value);
-    year = parseInt(document.getElementById("year").value);
-    month = parseInt(document.getElementById("month").value);
-    dayOfMonth = parseInt(document.getElementById("monthday").value);
-    if (century === "") {
-        alert("Input the century");
-        return false;
-    }  else if (year === "") {
-        alert("Input the correct year");
-        return false;
-    }  else if (month === "") {
-        alert("input the correct month");
-        return false;
+function readData() {
+    var dd = document.getElementById("dd").value; //get value date input
+    var dd = document.getElementById("mm").value; //get value month input
+    var dd = document.getElementById("yy").value; //get value year input
+    var male = document.getElementById("male").checked; //return true if male button selected and false if unselected
+    var female = document.getElementById("female").checked; //return true if female button selected and false if unselected
+    var days = document.getElementById["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"] //array for days
+    var maleNames = document.getElementById["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yao", "Kofi", "Kwame"] //array for maleNames
+    var femaleNames = document.getElementById["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"] //array for femaleNames
+    var birthDate = new Date(mm + '/' + dd + '/' + yy);
 
-    }  else if (dayOfMonth === "") {
-        alert("input the correct date");
-        return false;
-    }
+    var daysOfTheWeek = birthDate.getDate(); //calculation of days of the week. 
+
+
 
 }
